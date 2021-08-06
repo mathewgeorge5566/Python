cb6f6001883ea946aee91d32782cef5befc5c83d
Mathew George <84261994+mathewgeorge5566@users.noreply.github.com>
2021-08-06 18:55:40 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+'Mathew'
+
+
+# In[2]:
+
+
+"George"
+
+
+# In[3]:
+
+
+print('Hello World ')
+print('Hello World ')
+print('\n')
+print('\n')
+print('Mathew George')
+
+
+# In[4]:
+
+
+s = "hello"
+s=s+' Mathew '
+print(s)
+
+
+# In[5]:
+
+
+s.upper()
+
+
+# In[6]:
+
+
+s.lower()
+
+
+# In[7]:
+
+
+s.split('W')
+
+
+# In[8]:
+
+
+s.split('g')
+
+
+# In[9]:
+
+
+s.split('w')
+
+
+# In[10]:
+
+
+s.split('mathew')
+
+
+# In[11]:
+
+
+s.split('Mathew')
+
+
+# In[ ]:
+
+
+
+
